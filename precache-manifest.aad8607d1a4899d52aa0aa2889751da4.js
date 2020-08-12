f2bc811cc81f78d3dc38bc1660caeb7f1965ec49
Michael Dunn <michael.g.dunn@btinternet.com>
2020-08-12 18:48:14 +0100

    Updates

---
+++ precache-manifest.aad8607d1a4899d52aa0aa2889751da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdb2bbdb87a1b21e326724dd71600b7",
+    "url": "/simple-todo-app2/index.html"
+  },
+  {
+    "revision": "72067afd3e7ccfda3da4",
+    "url": "/simple-todo-app2/static/css/main.d02db337.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app2/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app2/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72067afd3e7ccfda3da4",
+    "url": "/simple-todo-app2/static/js/main.c6175562.chunk.js"
+  },
+  {
+    "revision": "e13a18720cb8bf7bc95d",
+    "url": "/simple-todo-app2/static/js/runtime-main.a9a70b8b.js"
+  }
+]);
